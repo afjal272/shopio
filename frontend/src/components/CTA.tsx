136c0ea2ab59d0f618a4783f6fb1fe0864c2248d
afjal Ali <mdafjalali272@gmail.com>
2026-04-18 03:15:04 +0530

    improve results UI

--- frontend/src/components/CTA.tsx
+++ frontend/src/components/CTA.tsx
@@ -1,13 +1,13 @@
 export default function CTA() {
   return (
-    <section className="w-full py-24 bg-black text-white">
+    <section className="w-full py-24 bg-gray-200 text-white">
       <div className="max-w-4xl mx-auto px-6 text-center">
 
-        <h2 className="text-4xl font-bold">
+        <h2 className="text-4xl   text-black font-bold">
           Stop wasting time comparing.
         </h2>
 
-        <p className="mt-4 text-gray-400 text-lg">
+        <p className="mt-4 text-black text-lg">
           Let AI pick the best product for you in seconds.
         </p>
 
